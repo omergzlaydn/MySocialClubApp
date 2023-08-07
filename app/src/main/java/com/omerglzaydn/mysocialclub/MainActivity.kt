8f742c15a332715d4e3cc0670a71c62250c27c74
Ömer <omrgzlaydn@gmail.com>
2023-08-08 00:11:29 +0300

    inital commit

---
+++ app/src/main/java/com/omerglzaydn/mysocialclub/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.omerglzaydn.mysocialclub
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
